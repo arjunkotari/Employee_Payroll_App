2e083978593c4c2f3d78022835eeff52bcec4af2
arjunkotari <arjun.kotari123@gmail.com>
2021-09-02 16:21:17 +0530

    Added: Ability to create Employee Payroll Object On Save.

--- js/EmployeePayrollData.js
+++ js/EmployeePayrollData.js
@@ -22,3 +22,50 @@ window.addEventListener('DOMContentLoaded', (event) => {
         output.textContent = salary.value
     });
 });
+
+const save = () => {
+    try {
+        let employeePayrollData = createEmployeePayroll();
+    } catch (error) {
+        return;
+    }
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData();
+    try {
+        employeePayrollData.name = getInputValueById("#name");
+    } catch (error) {
+        setTextValue('.text-error', error);
+        throw error;
+    }
+    employeePayrollData.profilePic = getSelectedValues("[name = profile]").pop();
+    employeePayrollData.gender = getSelectedValues("[name = gender]").pop();
+    employeePayrollData.department = getSelectedValues("[name = department]");
+    employeePayrollData.salary = getInputValueById("#salary");
+    employeePayrollData.notes = getInputValueById("#notes");
+    let date = getInputValueById('#day')+" "+ getInputValueById('#month')+" "+getInputValueById('#year');
+    employeePayrollData.date = Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const getSelectedValues = (property) => {
+    let allItems = document.querySelectorAll(property);
+    let selectedItems = [];
+    allItems.forEach(item => {
+        if (item.checked)
+            selectedItems.push(item.value);
+    });
+    return selectedItems;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
